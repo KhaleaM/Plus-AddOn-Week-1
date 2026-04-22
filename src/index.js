c3a7b613da7e13bc7553713fbdf1b6c2fe0d4cef
KhaleaM <106846809+KhaleaM@users.noreply.github.com>
2026-04-21 20:58:56 -0500

    added "moments" cdn, "moments timezone" cdn, linked "index.js" to "index.html + corrected "id" to "value"

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
-function cityTimezone(event) {
+function cityTime(event) {
   if (event.target.value === "paris") {
     alert(
       moment()
         .tz("Europe/Paris")
-        .format("[It is] dddd, MMMM d, YYYY h:m A [in Europe/Paris]"),
+        .format("[It is] dddd, MMMM Do, YYYY h:m A [in Europe/Paris]"),
     );
   }
   if (event.target.value === "sydney") {
     alert(
       moment()
         .tz("Australia/Sydney")
-        .format("[It is] dddd, MMMM d, YYYY h:m A [in Australia/Sydney]"),
+        .format("[It is] dddd, MMMM Do, YYYY h:m A [in Australia/Sydney]"),
     );
   }
   if (event.target.value === "tokyo") {
     alert(
       moment()
-        .tz("Japan/Tokyo")
-        .format("[It is] dddd, MMMM d, YYYY h:m A [in Japan/Tokyo]"),
+        .tz("Asia/Tokyo")
+        .format("[It is] dddd, MMMM Do, YYYY H:mm A [in Japan/Tokyo]"),
     );
   }
 }
 let citiesSelect = document.querySelector("#cities");
-citiesSelect.addEventListener("change", cityTimezone);
+citiesSelect.addEventListener("change", cityTime);
